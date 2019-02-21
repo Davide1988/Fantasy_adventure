1af866eb80bca8566dc50c186f51a060d31618d6
Davide Venditti <davide_335@msn.com>
2019-02-21 11:14:06 +0000

    add starting point for player class

---
+++ src/main/java/Player.java
@@ -0,0 +1,30 @@
+public class Player {
+
+
+    private int health;
+    private boolean isDeath;
+
+
+
+    public Player(int health, boolean isDeath){
+        this.health = health;
+        this.isDeath = isDeath;
+    }
+
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public boolean isDeath() {
+        return isDeath;
+    }
+
+    public void setDeath(boolean statust) {
+        isDeath = status;
+    }
+}
