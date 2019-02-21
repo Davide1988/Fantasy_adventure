094825c2d9688fcde8816055fcef80b148e6a268
Thomas Kendrick <tommkendrick@gmail.com>
2019-02-21 14:45:50 +0000

    added and tested room package

---
+++ src/main/java/room/Room.java
@@ -0,0 +1,37 @@
+package room;
+
+import beasts.Beast;
+import fantasyAdventure.Player;
+
+public class Room {
+    Player player;
+    Beast beast;
+    int loot;
+
+    public Room(Player player, Beast beast, int loot){
+       this.player = player;
+       this.beast = beast;
+       this.loot = loot;
+    }
+
+    public int getLoot() {
+        return loot;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Beast getBeast() {
+        return beast;
+    }
+
+    public void playerTurn(){
+        this.beast.takeDamage(this.player.attack());
+    }
+}
+
+
+
+
+
---
+++ src/test/java/RoomTest.java
@@ -0,0 +1,50 @@
+import beasts.Beast;
+import beasts.Dragon;
+import fantasyAdventure.Player;
+import fantasyAdventure.fighter.Dwarve;
+import items.Weapon;
+import org.junit.Before;
+import org.junit.Test;
+import room.Room;
+
+import static org.junit.Assert.assertEquals;
+
+public class RoomTest {
+    Room room;
+    Beast beast;
+    Player player;
+    @Before
+    public void before(){
+        player = new Dwarve(Weapon.FLAIL);
+        beast = new Dragon(3,3,100);
+        room = new Room(player, beast, 5);
+    }
+
+    @Test
+    public void roomHasPlayer(){
+        assertEquals(player, room.getPlayer());
+    }
+
+    @Test
+    public void roomHasBeast(){
+        assertEquals(beast, room.getBeast());
+    }
+
+    @Test
+    public void roomHasLoot(){
+        assertEquals(5, room.getLoot());
+    }
+
+    @Test
+    public void playerCanTakeTurn(){
+        room.playerTurn();
+        assertEquals(96, room.getBeast().getHealth());
+    }
+
+    @Test
+    public void dwarfspecialplayerCanTakeTurn(){
+        room.getPlayer().takeDamage(75);
+        room.playerTurn();
+        assertEquals(92, room.getBeast().getHealth());
+    }
+}
