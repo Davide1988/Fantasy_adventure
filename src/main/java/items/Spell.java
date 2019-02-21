745d6070bf28517042f713e57ed48ec21dc3b84b
Thomas Kendrick <tommkendrick@gmail.com>
2019-02-21 11:55:52 +0000

    Added some spells

---
+++ src/main/java/items/Spell.java
@@ -0,0 +1,17 @@
+package items;
+
+public enum Spell {
+
+    MAGICMISSILE(5),
+    FIREBALL(7),
+    CHAINLIGHTNING(8),
+    SHOCKINGGRASP(7),
+    METEORS(9),
+    POWERWORDDEATH(999);
+
+    int damage;
+
+    Spell(int damage) {
+        this.damage = damage;
+    }
+}
